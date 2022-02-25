eb30bba8f3267553248349cb863ded6cde88c487
Shinigami <stopputinshinigami@gmail.com>
2022-02-25 18:34:20 +0100

    [feat] - add useeffect

--- src/components/DisplayName.tsx
+++ src/components/DisplayName.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import { BorderedBox } from "./styles";
 
 export const DisplayName = ({ name }: { name: string }) => {
+  useEffect(() => {}, []);
+
   return (
     <BorderedBox>
       <div style={{ fontWeight: 600 }}> {name}</div>
